3b5d28bdc9ccc515fc5f58a161e4934f250bb081
Santhosh <santhosh_nampally@epam.com>
2019-07-17 12:58:52 +0530

    Initial Commit

---
+++ Notes/Notes.h
@@ -0,0 +1,19 @@
+//
+//  Notes.h
+//  Notes
+//
+//  Created by Santhosh Nampally on 17/07/19.
+//  Copyright © 2019 Santhosh Nampally. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Notes.
+FOUNDATION_EXPORT double NotesVersionNumber;
+
+//! Project version string for Notes.
+FOUNDATION_EXPORT const unsigned char NotesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Notes/PublicHeader.h>
+
+
